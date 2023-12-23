d2137ea32b89dd8f9f0e6b53a0422707a112cf01
Igor0763 <Igor763763763@yandex.ru>
2023-12-23 23:42:57 +0400

    first commit

---
+++ src/test/java/data/Product.java
@@ -0,0 +1,13 @@
+package data;
+
+public enum Product {
+    IPHONE("Айфон"),
+    XIAOMI("Ксиоми"),
+    SAMSUNG("Самсунг");
+public final String description;
+
+    Product(String description) {
+        this.description = description;
+    }
+}
+
---
+++ src/test/java/tests/TestBases.java
@@ -0,0 +1,16 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBases {
+    @BeforeAll
+    static void openSite() {
+        open("https://www.citilink.ru/");
+        Configuration.holdBrowserOpen = true;
+        Configuration.pageLoadStrategy = "eager";
+
+    }
+}
